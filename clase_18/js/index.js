2844c6463776e6397f21df9165977c56bd42ade5
Carlos Perren <idux.net@gmail.com>
2023-12-08 09:34:32 -0300

    Primeros pasos Javascript

---
+++ clase_18/js/index.js
@@ -0,0 +1,30 @@
+/**
+* Elementos básicos de lenguaje
+*/
+
+// Constantes y variables
+// Constante: declaro y asigno
+const PI = 3.14
+const K = 3
+
+// Variable: declaro y asigno
+let saldo = 20
+
+const calculo = PI * saldo
+
+/* console.log(PI)
+console.log(saldo)
+console.log(calculo)
+console.log(PI * K) */
+
+// Ciclos
+let limite = 100
+for (let ciclo1 = 1; ciclo1 <= limite; ciclo1 = ciclo1 + 1) {
+    console.log(ciclo1)
+}
+
+/* console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5) */
