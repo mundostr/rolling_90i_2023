d2879acc6ebe5a6260f6358af0d90533dcc90e26
Carlos Perren <idux.net@gmail.com>
2023-12-13 11:39:36 -0300

    Ctes, variables, ciclos, condiciones, operadores

---
+++ clase_19/js/ciclos.js
@@ -0,0 +1,50 @@
+/**
+ * Elementos básicos de lenguaje
+ * Ciclos
+ * 
+ * Un ciclo (loop / bucle) nos permite repetir instrucciones.
+ * Utiliza una variable auxiliar interna, o una condición, para determinar cómo se repite.
+ * 
+ * De esa forma, podemos tener:
+ * - ciclos finitos: x cantidad de veces.
+ * - ciclos infinitos: durante toda la ejecución del código.
+ * - ciclos indefinidos: mientras se cumpla una condición o hasta que suceda cierto evento.
+*/
+
+// Si debo imprimir los números del 1 al 10, por supuesto puedo hacerlo a mano:
+// pero lógicamente no es muy eficiente, sobre todo a medida que aumenta la cantidad
+console.log('Manualmente')
+console.log(1)
+console.log(2)
+console.log(3)
+console.log(4)
+console.log(5)
+console.log(6)
+console.log(7)
+console.log(8)
+console.log(9)
+console.log(10)
+
+// Un ciclo nos da un bloque de código ordenado que podemos repetir
+// Vemos que no solo el código ha quedado más compacto, sino también más flexible,
+// ya que modificando LIMITE, podemos variar la cantidad de veces que se repite.
+console.log('Utilizando un ciclo FINITO')
+const LIMITE = 10
+for (let c1 = 1; c1 <= LIMITE; c1++) {
+    console.log(c1)
+}
+
+// Con while() o do while(), podemos generar un ciclo INFINITO o INDEFINIDO,
+// según la condición que utilicemos.
+// En este caso indicamos que el ciclo debe repetirse mientras ingreso sea
+// distinto de 1, 2 y 3.
+// Mientras coloquemos cualquier valor que no sea 1, 2 o 3, el ciclo seguirá
+// y volverá a solicitar otro valor.
+// Cuando coloquemos 1, 2 o 3, el ciclo se interrumpirá y el sistema continuará
+// con la siguiente instrucción por fuera (console.log(ingreso))
+console.log('Utilizando un ciclo INFINITO o INDEFINIDO')
+let ingreso = ''
+do {
+    ingreso = parseInt(prompt('Ingresar opción (1 | 2 | 3'))
+} while (ingreso != 1 && ingreso != 2 && ingreso != 3)
+console.log(ingreso)
---
+++ clase_19/js/constantes_variables.js
@@ -0,0 +1,19 @@
+/**
+ * Elementos básicos de lenguaje
+ * Constantes y variables
+ * 
+ * Una constante o variable, nos permite almacenar UN dato.
+ * La identificamos con un nombre, idealmente corto y relacionado a lo que contiene.
+*/
+
+// Constante: declaro con la palabra reservada const
+const PI = 3.14
+
+// Variable: declaro con la palabra reservada let
+let saldo = 200
+
+// console.log nos permite mostrar mensajes en la consola de inspección del navegador
+// (Herramientas de desarrollo -> F12)
+console.log(PI)
+console.log(saldo)
+console.log(PI * saldo)
